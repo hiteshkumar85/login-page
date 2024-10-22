821260928d4c696cd092cf296d2316e85dc4ee8f
VIJAY <vsaini47428@gmail.com>
2024-10-22 15:21:17 +0530

    test

---
+++ main.js
@@ -0,0 +1,6 @@
+let a = prompt("enter value of a");
+let b = prompt("enter value of b");
+let c = localStorage.setItem("a","b")
+console.log(c);
+localStorage.removeItem(c)
+let user = JSON.stringify.localStorage()
